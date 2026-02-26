064239ed89d7ef9d5f1cf3426cd6ea1bbf07828c
Marianvsf <marianvsf@gmail.com>
2026-02-26 16:15:58 -0400

    debug deploy

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
-echo "<h1>Prueba de vida</h1>";
-echo "PHP Version: " . phpversion();
-echo "<br>Extensión PDO Postgres: " . (extension_loaded('pdo_pgsql') ? 'Instalada' : 'NO INSTALADA');
+// Forzar visualización de errores
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "<h1>Prueba de vida del servidor</h1>";
+echo "PHP está funcionando correctamente.<br>";
+
+// Verificar si las extensiones de Postgres están activas
+echo "PDO Postgres: " . (extension_loaded('pdo_pgsql') ? 'INSTALADA' : 'FALTA') . "<br>";
+
 exit;
